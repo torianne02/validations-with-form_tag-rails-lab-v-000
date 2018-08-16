ad5747baa24fbd921cf23c5a9fa81df931f4b970
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-08-16 20:34:38 +0000

    automatically backed up by learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -8,7 +8,8 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    @author = Author.create(author_params)
+    @author = Author.new(author_params)
+    if @author.save
 
     redirect_to author_path(@author)
   end
